62b7e2978405ad5d6cb6c79d21c02159277daf6d
Koffi Cobbin <52169278+Koffi-Cobbin@users.noreply.github.com>
2021-02-11 15:43:02 +0000

    Create database.py

---
+++ src/common/database.py
@@ -0,0 +1,36 @@
+__author__ = 'cobbin'
+
+import pymongo
+import os
+
+class Database(object):
+    URI = os.environ.get("MONGODB_URI")
+    DATABASE = None
+
+    @staticmethod
+    def initialize():
+        client = pymongo.MongoClient(Database.URI)
+        Database.DATABASE = client['bgq0ggarpfpksq4'] 
+
+    @staticmethod
+    def insert(collection, data):
+        Database.DATABASE[collection].insert(data)
+
+    @staticmethod
+    def find(collection, query):
+        return Database.DATABASE[collection].find(query)
+
+    @staticmethod
+    def find_one(collection, query):
+        return Database.DATABASE[collection].find_one(query)
+
+    @staticmethod
+    def update(collection, query, data):
+        Database.DATABASE[collection].update(query, data, upsert=True)
+
+    @staticmethod
+    def remove(collection, query):
+        Database.DATABASE[collection].remove(query)
+
+    
+        
